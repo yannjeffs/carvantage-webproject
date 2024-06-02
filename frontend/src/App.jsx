c3537690cb431a3b3c563bf0855785c8bd8d53cd
yannjeffs <yangoune10@gmail.com>
2024-06-02 16:57:08 +0100

    filling of footer.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import AppRoutes from "./AppRoutes";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <>
       <Header />
       <AppRoutes />
+      <Footer />
     </>
   )
 }
